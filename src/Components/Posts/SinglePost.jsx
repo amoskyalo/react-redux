f814ed74f277460d8b1d7ecb256b04e663a2ff12
amoskyalo <amoskyalo927@gmail.com>
2022-11-30 16:33:49 +0300

    added back-home button to single post

--- src/Components/Posts/SinglePost.jsx
+++ src/Components/Posts/SinglePost.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const SinglePost = () => {
   const { postId } = useParams();
@@ -11,11 +11,18 @@ const SinglePost = () => {
   );
 
   return (
-    <div className="flex flex-col border rounded p-2">
+    <div className="flex flex-col space-y-4">
+      <div className="flex flex-col border rounded p-2">
       <h2 className="text-lg font-semibold">
-        {posts.title} <span className="block text-sm font-normal italic">by {posts.author}</span>
+        {posts.title} <span className="block text-sm font-normal italic">by {posts.author},</span>
       </h2>
       <p className="my-5">{posts.content}</p>
+    </div>
+    <div className="flex justify-start ml-5">
+      <Link to="/" className="bg-navColor py-1 px-5 rounded text-white text-base">
+        Back
+      </Link>
+    </div>
     </div>
   );
 };
